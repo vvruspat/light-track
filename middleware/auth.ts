0836f0286c723fd7d9f6fc9f8e3b6a0e3c30b588
Alexander Kolesov <vvruspat@gmail.com>
2025-02-07 20:21:02 +0100

    auth test

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,13 +1,10 @@
 // import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 export default defineNuxtRouteMiddleware((_to, _from) => {
-//   const { initDataRaw, initData } = retrieveLaunchParams();
-
-//   console.log("mw: initDataRaw: ", initDataRaw);
-//   console.log("mw: initData: ", initData);
-
+  //   const { initDataRaw, initData } = retrieveLaunchParams();
+  //   console.log("mw: initDataRaw: ", initDataRaw);
+  //   console.log("mw: initData: ", initData);
   // const user = useSupabaseUser();
-
   // if (!user.value) {
   //   return navigateTo('/login');
   // }

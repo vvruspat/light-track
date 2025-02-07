aac29f2719d33a73a5ef02709d421528f90335c2
Alexander Kolesov <vvruspat@gmail.com>
2025-02-07 21:14:19 +0100

    auth test

---
+++ components/TgAuth.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { retrieveLaunchParams } from '@telegram-apps/sdk';
+
+const { initDataRaw } = retrieveLaunchParams();
+
+console.log('initDataRaw', initDataRaw);
+</script>
+
+<template>
+  <div>
+    Auth
+  </div>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,60 +1,9 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import { retrieveLaunchParams } from "@telegram-apps/sdk";
-
-definePageMeta({
-  title: "Login",
-  description: "Login to your account",
-  middleware: ["auth"],
-});
-
-const { initDataRaw, initData } = retrieveLaunchParams();
-
-console.log("initDataRaw: ", initDataRaw);
-console.log("initData: ", initData);
-// const supabase = useSupabaseClient();
-
-// const email = ref("");
-// const password = ref("");
-const authError = ref("");
-
-// const router = useRouter();
-
-const handleSubmit = async (event: Event) => {
-  event.preventDefault();
-
-  console.log("Form submitted");
-
-  // const { error } = await supabase.auth.setSession({ access_token, refresh_token });
-
-  // const { error } = await supabase.auth.signInWithPassword({
-  //   email: email.value,
-  //   password: password.value,
-  // });
-
-  // if (error) {
-  //   authError.value = error.message;
-  // } else {
-  //   router.push("/");
-  // }
-};
+import TgAuth from '~/components/TgAuth.vue';
 </script>
 
 <template>
-  <UContainer>
-    <UCard>
-      <StackContainer justify="center" align-items="center">
-        <template #header>
-          <h1>Login</h1>
-        </template>
-        <UAlert v-if="authError" type="error">{{ authError }}</UAlert>
-        <form @submit.prevent="handleSubmit">
-          <!-- <UInput v-model="email" type="text" placeholder="Username" />
-          <UInput v-model="password" type="password" placeholder="Password" />
-          <button type="submit">Login</button> -->
-          <UButton type="submit">Login</UButton>
-        </form>
-      </StackContainer>
-    </UCard>
-  </UContainer>
+  <ClientOnly>
+    <TgAuth />
+  </ClientOnly>
 </template>

1c131b0556cc5f5bc8effd9474092779cf2a294f
Alexander Kolesov <vvruspat@gmail.com>
2025-02-08 00:34:50 +0100

    auth test

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,6 @@
-
 export default defineNuxtRouteMiddleware((to, _from) => {
   console.log("to", to);
   if (import.meta.client && to.path === "/login") {
-    
     const hash = to.hash?.slice(1);
     const params = new URLSearchParams(hash);
     console.log(params.entries());

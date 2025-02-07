c67b4ff83030e4aeec7b65d18f84ee4a08a1f6f2
Alexander Kolesov <vvruspat@gmail.com>
2025-02-08 00:07:35 +0100

    auth test

--- middleware/authenticate.ts
+++ middleware/authenticate.ts
@@ -1,10 +1,10 @@
 import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 export default defineNuxtRouteMiddleware((to, _from) => {
-    console.log("to", to);
-    if (import.meta.client && to.path === "/login") {
-        const { initDataRaw, initData } = retrieveLaunchParams();
+  console.log("to", to);
+  if (import.meta.client && to.path === "/login") {
+    const { initDataRaw, initData } = retrieveLaunchParams();
 
-        console.log("initDataRaw", initDataRaw, initData);
-    }
-})
+    console.log("initDataRaw", initDataRaw, initData);
+  }
+});

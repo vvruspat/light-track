d2a5a6b39c03f36fd9e05cf521cf27755f33f116
Alexander Kolesov <vvruspat@gmail.com>
2025-02-08 21:11:27 +0100

    auth test

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,13 @@
+import { retrieveLaunchParams } from '@telegram-apps/bridge';
+
 export default defineNuxtRouteMiddleware((to, _from) => {
   console.log("to", to);
   if (import.meta.client && to.path === "/login") {
-    const hash = to.hash?.slice(1);
-    const params = new URLSearchParams(hash);
-    console.log(params.entries());
+
+    const launchParams = retrieveLaunchParams();
+    // const hash = to.hash?.slice(1);
+    // const params = new URLSearchParams(hash);
+    
+    console.log("launchParams", launchParams); 
   }
 });

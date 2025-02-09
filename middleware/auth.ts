82473b4ad7accdb207b8fd2f1a26cdf337952930
Alexander Kolesov <vvruspat@gmail.com>
2025-02-10 00:10:24 +0100

    auth test

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,15 @@
 import { retrieveLaunchParams } from "@telegram-apps/bridge";
 
 export default defineNuxtRouteMiddleware((to, _from) => {
-  // const authStore = useAuthStore();
+  const authStore = useAuthStore();
+
   if (import.meta.client && to.path === "/login") {
     const launchParams = retrieveLaunchParams();
 
     console.log("launchParams", launchParams);
+
+    if (launchParams.tgWebAppData && launchParams.tgWebAppData.user) {
+      authStore.login(launchParams);
+    }
   }
 });
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,22 +1,23 @@
 import { defineStore } from "pinia";
+import type { RetrieveLPResult } from "@telegram-apps/bridge";
 
-type TAppTheme = {
-  accent_text_color: string;
-  bg_color: string;
-  bottom_bar_bg_color: string;
-  button_color: string;
-  button_text_color: string;
-  destructive_text_color: string;
-  header_bg_color: string;
-  hint_color: string;
-  link_color: string;
-  secondary_bg_color: string;
-  section_bg_color: string;
-  section_header_text_color: string;
-  section_separator_color: string;
-  subtitle_text_color: string;
-  text_color: string;
-};
+// type TAppTheme = {
+//   accent_text_color: string;
+//   bg_color: string;
+//   bottom_bar_bg_color: string;
+//   button_color: string;
+//   button_text_color: string;
+//   destructive_text_color: string;
+//   header_bg_color: string;
+//   hint_color: string;
+//   link_color: string;
+//   secondary_bg_color: string;
+//   section_bg_color: string;
+//   section_header_text_color: string;
+//   section_separator_color: string;
+//   subtitle_text_color: string;
+//   text_color: string;
+// };
 
 type TUser = {
   allows_write_to_pm: boolean;
@@ -28,7 +29,7 @@ type TUser = {
   username: string;
 };
 
-type TAppData = {
+export type TAppData = {
   auth_date: string;
   chat_instance: string;
   chat_type: string;
@@ -37,13 +38,13 @@ type TAppData = {
   user: TUser;
 };
 
-type TMiniAppInit = {
-  tgWebAppBotInline: boolean;
-  tgWebAppData: TAppData;
-  tgWebAppPlatform: string;
-  tgWebAppThemeParams: TAppTheme;
-  tgWebAppVersion: string;
-};
+// type TMiniAppInit = {
+//   tgWebAppBotInline: boolean;
+//   tgWebAppData: TAppData;
+//   tgWebAppPlatform: string;
+//   tgWebAppThemeParams: TAppTheme;
+//   tgWebAppVersion: string;
+// };
 
 type AuthState = {
   currentUser: (TUser & { chatId: TAppData["chat_instance"] }) | null;
@@ -54,7 +55,7 @@ type AuthGetters = {
 };
 
 type AuthActions = {
-  login: (appInitData: TMiniAppInit) => Promise<void>;
+  login: (appInitData: RetrieveLPResult) => Promise<void>;
   logout: () => void;
 };
 
@@ -84,19 +85,25 @@ export const useAuthStore = defineStore<
   },
 
   actions: {
-    async login(appInitData: TMiniAppInit) {
+    async login(appInitData: RetrieveLPResult) {
       try {
-        await $fetch("/api/login", {
+        const data = await $fetch("/api/login", {
           method: "POST",
-          body: JSON.stringify(appInitData.tgWebAppData),
+          body: JSON.stringify(appInitData),
         });
 
-        this.currentUser = {
-          ...appInitData.tgWebAppData.user,
-          chatId: appInitData.tgWebAppData.chat_instance,
-        };
+        console.log("data: ", data);
 
-        navigateTo("/dashboard");
+        if (appInitData.tgWebAppData) {
+          // this.currentUser = {
+          //   ...appInitData.tgWebAppData.user,
+          //   chatId: appInitData.tgWebAppData.chat_instance ?? "0",
+          // };
+
+          console.log("this.currentUser: ", this.currentUser);
+
+          navigateTo("/dashboard");
+        }
       } catch (error) {
         console.error(error);
         navigateTo("/error");

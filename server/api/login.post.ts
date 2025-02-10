7852feddc3c6ba868d04ea72043f576a4e78e17c
Alexander Kolesov <vvruspat@gmail.com>
2025-02-10 14:16:27 +0100

    auth test

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -10,10 +10,8 @@ export default defineEventHandler(async (event): Promise<LoginPostResponse> => {
       ...body,
       user: JSON.stringify(body.user),
     };
-
-    throw Error("data: " + JSON.stringify(data));
-
-    validate3rd(new URLSearchParams(data), Number(botId));
+    
+    validate3rd((new URLSearchParams(data)).toString(), Number(botId));
   } catch (e) {
     throw createError({
       statusCode: 400,

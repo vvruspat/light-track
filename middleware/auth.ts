cb703a3eb650932ffa5c7f952632032e1a1c3a5c
Alexander Kolesov <vvruspat@gmail.com>
2025-02-10 10:51:00 +0100

    auth test

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,14 @@
 import { retrieveLaunchParams } from "@telegram-apps/bridge";
 
 export default defineNuxtRouteMiddleware((to, _from) => {
-  const authStore = useAuthStore();
-
   if (import.meta.client && to.path === "/login") {
     const launchParams = retrieveLaunchParams();
 
     console.log("launchParams", launchParams);
 
     if (launchParams.tgWebAppData && launchParams.tgWebAppData.user) {
+      const authStore = useAuthStore();
+
       authStore.login(launchParams);
     }
   }

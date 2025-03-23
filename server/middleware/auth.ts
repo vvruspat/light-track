c6e70416197faa2e8b5e81618f5ab8ca42e7caef
Alexander Kolesov <vvruspat@gmail.com>
2025-03-24 00:09:15 +0100

    make auth middleware only serverside

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,34 +2,36 @@ import useLightTrackSession from "@/utils/useLightTrackSession";
 import { NON_PROTECTED_API } from "@/constants/non-protected-api";
 
 export default defineEventHandler(async (event) => {
-  const url = getRequestURL(event).pathname;
+  if (import.meta.server) {
+    const url = getRequestURL(event).pathname;
 
-  if (
-    !url.includes("/api") ||
-    url.includes("/api/_nuxt") ||
-    NON_PROTECTED_API.includes(url)
-  ) {
-    return;
-  }
+    if (
+      !url.includes("/api") ||
+      url.includes("/api/_nuxt") ||
+      NON_PROTECTED_API.includes(url)
+    ) {
+      return;
+    }
 
-  try {
-    const { chatId, ...user } = useLightTrackSession(event);
+    try {
+      const { chatId, ...user } = useLightTrackSession(event);
 
-    if (!user) {
+      if (!user) {
+        return {
+          statusCode: 401,
+          statusMessage: "Unauthorized",
+          message: "Authentication is required",
+        };
+      } else {
+        event.context.user = user;
+        event.context.chatId = chatId;
+      }
+    } catch (e) {
       return {
         statusCode: 401,
         statusMessage: "Unauthorized",
-        message: "Authentication is required",
+        message: (e as Error).message,
       };
-    } else {
-      event.context.user = user;
-      event.context.chatId = chatId;
     }
-  } catch (e) {
-    return {
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-      message: (e as Error).message,
-    };
   }
 });

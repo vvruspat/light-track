30791c38991a4624c412302c43dcc33cfaf63996
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-04-16 23:13:26 +0200

    biome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
   vite: {
     server: {
       allowedHosts: [
-        "fca4-77-251-156-81.ngrok-free.app",
+        "8232-77-251-156-81.ngrok-free.app",
         "light-track.vercel.app",
       ],
     },
